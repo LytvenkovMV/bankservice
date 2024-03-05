aafc95edc85e7819c18ebcd6c08e01ebc1657420
Work PC <example@mail.com>
2024-03-05 16:48:33 +0300

    Developing ClientController

--- src/main/java/com/example/bankservice/controller/ClientController.java
+++ src/main/java/com/example/bankservice/controller/ClientController.java
@@ -1,6 +1,7 @@
 package com.example.bankservice.controller;
 
 import com.example.bankservice.dto.AddClientRequestDto;
+import com.example.bankservice.service.ClientService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/clients")
 public class ClientController {
+
+    private ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
 
     @ApiOperation(value = "Добавить клиента")
     @PostMapping
